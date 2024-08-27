abfe28fce329807c1bac86fa3806f313d8d715b9
Kira_Rapota <125643020+Kr68l@users.noreply.github.com>
2024-08-27 11:56:38 +0000

    edits

--- Anagrams.py
+++ Anagrams.py
@@ -1,13 +1,31 @@
-import re
+from string import punctuation, digits
 
-def reverse_words(text):
-  def reverse_and_join(match):
-    return ''.join(reversed(match.group()))
-  return re.sub(r"\w+", reverse_and_join, text)
+def reversed_text(text):
+    lst = []
+    qw = set.union(set(punctuation), set(digits))
 
-if __name__ == "__main__":
-  cases = ["abcd efgh", "a1bcd efg!h"]
-  for case in cases:
-    reversed_text = reverse_words(case)
-    print(reversed_text)
+    for word in text.split(' '):
+        sort = [i for i in word if i not in qw]
 
+        for i in word:
+            if i not in qw: 
+                lst.append(sort.pop())
+            else:
+                lst.append(i)
+              
+        lst.append(' ')
+
+    return ''.join(lst[:len(lst)-1])
+
+
+if __name__ == '__main__':
+    cases = [
+        ("abcd efgh", "dcba hgfe"),
+        ("a1bcd efg!h", "d1cba hgf!e"),
+        (" ", " "), 
+    ]
+
+    for text, expected_output in cases:
+        assert reversed_text(text) == expected_output
+
+    print(reversed_text(input("Please enter the text: ")))
